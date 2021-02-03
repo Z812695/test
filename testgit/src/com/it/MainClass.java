8377fa79f1e50da754558f24209d0b12fd39cbf3
z <812424695@qq.com>
2021-02-03 20:30:27 +0800

    项目初始环境

---
+++ testgit/src/com/it/MainClass.java
@@ -0,0 +1,11 @@
+package com.it;
+
+/**
+ * @Author Zz
+ * @Date 2021/2/3 16:25
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
